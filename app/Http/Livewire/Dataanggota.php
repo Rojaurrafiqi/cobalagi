02a1b13a451bf312909e9978fb79c4ff1a8e42da
Rojaur Rafiqi <rojaurrafiqi25@gmail.com>
2022-01-25 09:46:28 +0700

    coba livewire

---
+++ app/Http/Livewire/Dataanggota.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\perpustakaan;
+
+class Dataanggota extends Component
+{
+
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $searchTerm;
+    public $deleteId = '';
+
+    public function render()
+    {
+        $searchTerm = '%'.$this->searchTerm.'%';
+        return view('livewire.dataanggota',[
+            'perpustakaan' => perpustakaan::where('namalengkap','like', $searchTerm)->paginate(5)
+        ]);
+    }    
+
+    public function deleteId($id)
+    {
+        $this->deleteId = $id;
+    }
+
+    public function delete()
+    {
+        perpustakaan::find($this->deleteId)->delete();
+        session()->flash('message', 'Anggota berhasil dihapus.');
+    }
+
+
+}
